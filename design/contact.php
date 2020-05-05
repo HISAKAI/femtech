4bb13ea968c677517e5a5d44cf6866acd06cb35c
Hirotake Sakai <hirotake1120@yahoo.co.jp>
2020-05-05 22:43:11 +0900

    フッター年号の自動更新対応。

--- design/contact.php
+++ design/contact.php
@@ -9,7 +9,7 @@
 <div class="container">
 	<div class="topbar"></div>
 	<div class="header_wrap">
-		<div class="header">	
+		<div class="header">
 			<div class="header_top  clearfix">
 				<div class="header_msg">
 					発想を変えて不可能を可能に。 "Changing ideas, Make it possible."
@@ -25,7 +25,7 @@
 			</div>
 		</div>
 	</div>
-	
+
 	<div class="menu_bar">
 		<div class="menu_wrap clearfix">
 			<img class="logo2" src="img/femtech_logo_2.png" alt="femtech_logo_2" width="79" height="35" />
@@ -42,35 +42,35 @@
 	<div class="contents_wrap">
 		<div class="contact_message">
 <?php
-	mb_language("Ja");
-	mb_internal_encoding("UTF-8");
-	
-	//nullチェックとメール送信実行処理
-	if(!empty($_POST["name"]) && !empty($_POST["mail"]) && !empty($_POST["inquiry"])){
-		$comname=$_POST["comname"];
-		$depname=$_POST["depname"];
-		$name=$_POST["name"];
-		$address=$_POST["address"];
-		$telephone=$_POST["telephone"];
-		$mail=$_POST["mail"];
-		$subtitle=$_POST["subtitle"];
-		$inquiry=$_POST["inquiry"];
-		
-		//入力チェック
-		if(mb_strlen($name) >= 100){
-			echo "氏名は100文字以内で入力して下さい。/ Input name in 100 Charactors.";
-		}else if(mb_strlen($mail) >= 100){
-			echo "メールアドレスは100文字以内で入力して下さい。/ Input E-mail in 100 Charactors.";
-		}else if(mb_strlen($inquiry) >= 4000){
-			echo "問い合わせ内容は4000文字以内で入力して下さい。 / Input Inquiry in 4000 Charactors.";
-		}else{
-		
+mb_language("Ja");
+mb_internal_encoding("UTF-8");
+
+//nullチェックとメール送信実行処理
+if (!empty($_POST["name"]) && !empty($_POST["mail"]) && !empty($_POST["inquiry"])) {
+	$comname = $_POST["comname"];
+	$depname = $_POST["depname"];
+	$name = $_POST["name"];
+	$address = $_POST["address"];
+	$telephone = $_POST["telephone"];
+	$mail = $_POST["mail"];
+	$subtitle = $_POST["subtitle"];
+	$inquiry = $_POST["inquiry"];
+
+	//入力チェック
+	if (mb_strlen($name) >= 100) {
+		echo "氏名は100文字以内で入力して下さい。/ Input name in 100 Charactors.";
+	} else if (mb_strlen($mail) >= 100) {
+		echo "メールアドレスは100文字以内で入力して下さい。/ Input E-mail in 100 Charactors.";
+	} else if (mb_strlen($inquiry) >= 4000) {
+		echo "問い合わせ内容は4000文字以内で入力して下さい。 / Input Inquiry in 4000 Charactors.";
+	} else {
+
 		//メール送信実行
-		$mailto="info@femtech.co.jp";
-		$mailto2="hisakai@femtech.co.jp";
-		$mailto3="hyakusoku@femtech.co.jp";
-		$subject="【ホームページからの問い合わせ】会社名/部署名/氏名: " . $comname . '/' . $depname . '/' . $name;
-		$content='会社名/部署名/氏名: ' . $comname . '/' .$depname . '/' . $name . '
+		$mailto = "info@femtech.co.jp";
+		$mailto2 = "hisakai@femtech.co.jp";
+		$mailto3 = "hyakusoku@femtech.co.jp";
+		$subject = "【ホームページからの問い合わせ】会社名/部署名/氏名: " . $comname . '/' . $depname . '/' . $name;
+		$content = '会社名/部署名/氏名: ' . $comname . '/' . $depname . '/' . $name . '
 住所: ' . $address . '
 電話番号: ' . $telephone . '
 E-mail: ' . $mail . '
@@ -79,24 +79,26 @@ E-mail: ' . $mail . '
 ' . $subtitle . '
 【内容】
 ' . $inquiry;
-		$mailfrom="From:" .mb_encode_mimeheader("ホームページからの問い合わせ自動送信") ."<info@femtech.co.jp>";
-		mb_send_mail($mailto,$subject,$content,$mailfrom);
-		mb_send_mail($mailto2,$subject,$content,$mailfrom);
-		mb_send_mail($mailto3,$subject,$content,$mailfrom);
+		$mailfrom = "From:" . mb_encode_mimeheader("ホームページからの問い合わせ自動送信") . "<info@femtech.co.jp>";
+		mb_send_mail($mailto, $subject, $content, $mailfrom);
+		mb_send_mail($mailto2, $subject, $content, $mailfrom);
+		mb_send_mail($mailto3, $subject, $content, $mailfrom);
 
 		//	送信完了メッセージ
-		echo"問い合わせ内容を送信しました。/ Inquiry sent successfully.";
-		}
-	}else{
-		echo "全項目入力してください。/ Please input all fields";
+		echo "問い合わせ内容を送信しました。/ Inquiry sent successfully.";
+	}
+} else {
+	echo "全項目入力してください。/ Please input all fields";
 }
 ?>
 </div>
 	</div>
-	
 	<div class="footer">
-	
-		<div class="copyright">Copyright 2016(C) Femtech Corporation., Ltd. Allrights Reserved.</div>
+		<div class="copyright">
+			Copyright 2002 -
+			<script type="text/javascript">myDate = new Date() ;myYear = myDate.getFullYear ();document.write(myYear);</script>
+			(C) Femtech Corporation., Ltd. All Rights Reserved.
+		</div>
 	</div>
 </div>
 <script>
